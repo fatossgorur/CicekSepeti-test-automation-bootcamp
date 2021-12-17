cc5d75923c7a656ab25df6a277096231c2f86c3c
fatossgorur <55894683+fatossgorur@users.noreply.github.com>
2021-12-17 15:59:35 +0300

    Update User.cs

--- Week4/netcore-automation-test/user/User.cs
+++ Week4/netcore-automation-test/user/User.cs
@@ -6,8 +6,8 @@ namespace netcore_automation_test
 
     public class User
     {
-        private String email = "fatos.98gorur@hotmail.com";
-        private String password = "fats98gorur";
+        private String email = "**************";
+        private String password = "*****";
 
         public String GetEmail()
         {
